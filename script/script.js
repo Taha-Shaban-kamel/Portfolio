b275dafd1f0e83e512c1ccfa0c97b646dd0b7c50
Taha-Shaban-kamel <122947761+Taha-Shaban-kamel@users.noreply.github.com>
2023-09-17 23:38:33 +0300

    skills section restructure

--- script/script.js
+++ script/script.js
@@ -6,6 +6,7 @@ menuIcon.onclick = () => {
   navbar.classList.toggle('active');
 }
 
+
 // scroll secitons 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -31,9 +32,90 @@ window.onscroll = () => {
 }
 
 
+// skills section generate skills 
+const skillsSet = {
+  "frontend": {
+    "html5": "The HyperText Markup Language (HTML) is the standard markup language for creating web pages.",
+    "CSS-CSS3": "Cascading Style Sheets (CSS) is a style sheet language used to control the presentation of HTML documents.",
+    "Bootstrap": "A front-end CSS framework for developing responsive, mobile-first websites.",
+    "javaScript": "JavaScript is a programming language that is used to add interactivity to web pages.",
+    "React": "A JavaScript library for building user interfaces with reusable components."
+  },
+  "backend": {
+    "nodejs": "Node is used for server-side programming, making it possible for developers to use JavaScript for client-side and server-side code.",
+    "Express": "Express is a NodeJS framework that helps manage servers and routes.",
+    "Mongoose": "A MongoDB object modeling (ODM) library for Node.js that provides a simple and elegant way to interact with MongoDB databases.",
+    "MongoDB": "A NoSQL document database that is flexible, scalable, and easy to use.",
+    "FIREBASE":"Firebase is a mobile and web application development platform that provides a set of tools and services that front-end developers can use to build and deploy scalable and reliable applications."
+
+    
+  },
+  "tools": {
+    "git": "is a distributed version control system that tracks changes in any set of computer files.",
+    "gitHub":"is a cloud-based hosting service that helps developers store and manage their code, as well as collaborate on projects with other developers.",
+    "Figma": " is a cloud-based design platform that helps teams design and prototype digital products.",
+    "command-Line": "is a text-based user interface that allows users to interact with a computer by typing commands.",
+  }
+}
+
+
+const skillsList = document.querySelector('.skillsList');
+
+
+
+skillsList.addEventListener('click', (e) => {
+
+  let curentElement = e.target;
+  let skillscontainer = curentElement.nextElementSibling;
+ 
+  let curentSkils ;
+  //  generate my skills based on type of skills category 
+  if (curentElement.classList[1] === 'FrontEnd-skills') {
+    curentSkils = skillsSet.frontend;
+  }
+  else if (curentElement.classList[1] === 'back-End')
+    curentSkils = skillsSet.backend;
+  else if (curentElement.classList[1] === 'tools')
+    curentSkils = skillsSet.tools;
+  else
+    return;
+
+    const appendSkills = (skill,detail)=>{
+      let  li = document.createElement('li');
+      let  div = document.createElement('div');
+      let sikillDetailcontainer = document.createElement('div');
+      sikillDetailcontainer.classList.add('skillDetail');
+      let  p = document.createElement('p');
+      let hr = document.createElement('hr');
+      let img = document.createElement('img');
+      img.setAttribute("src",`image/${skill}.png`);
+      img.classList.add('skills-img');
+      li.classList.add('skill-content');
+      div.classList.add('skill');
+      p.classList.add('detail');
+      div.textContent = skill;
+      p.textContent = detail;
+      sikillDetailcontainer.appendChild(div);
+      sikillDetailcontainer.appendChild(p);
+      li.appendChild(img);
+      li.appendChild(sikillDetailcontainer);
+      skillscontainer.appendChild(li);
+      li.after(hr);
+    }
+
+  if (skillscontainer.innerHTML == '')
+    for (const [key, value] of Object.entries(curentSkils)) 
+      appendSkills(key,value);
+     else 
+    skillscontainer.innerHTML = '';
+  curentElement.firstElementChild.classList.toggle('bx-chevron-up')
+})
+
 
 // CopyRight 
 
 let year = document.querySelector("footer  .footer-text .year");
 
-year.innerHTML =  new Date().getFullYear();
+year.innerHTML = new Date().getFullYear();
+
+
